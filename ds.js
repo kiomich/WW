8f2ec4e8c669166498c42412afe859c1905c6417
kiomich <glazne097@gmail.com>
2025-03-27 22:23:10 +0200

    Delete ds.js

--- ds.js
+++
@@ -1,8 +0,0 @@
-// Добавляем переходы по кнопкам
-document.getElementById('steamBtn').onclick = function() {
-    window.location.href = 'https://steamcommunity.com/profiles/76561199528919632/';
-};
-
-document.getElementById('steamBtn2').onclick = function() {
-    window.location.href = 'https://steamcommunity.com/profiles/76561199655045336/';
-};

b3064b213894c43ff35556c0039335f90e48edbf
kiomich <glazne097@gmail.com>
2025-03-27 02:07:08 +0200

    Add files via upload

---
+++ ds.js
@@ -0,0 +1,8 @@
+// Добавляем переходы по кнопкам
+document.getElementById('steamBtn').onclick = function() {
+    window.location.href = 'https://steamcommunity.com/profiles/76561199528919632/';
+};
+
+document.getElementById('steamBtn2').onclick = function() {
+    window.location.href = 'https://steamcommunity.com/profiles/76561199655045336/';
+};
